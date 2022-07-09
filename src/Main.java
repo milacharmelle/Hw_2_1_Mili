7c176e0da4892fb7f49374fb74275051646de149
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-07-09 20:58:39 +0500

    git commit https://github.com/milacharmelle/Hw_2_1_Mili.git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int ticketPrice = 13_205;
+        int mili = 20;
+        int getMili = ticketPrice/mili;
 
+        System.out.println(getMili);
     }
 }
 
